af636ce3f3374cd2af6d8873cc907b16a38c7383
Christian Landes <iop90cwl@charter.net>
2016-04-27 19:44:44 -0700

    Added fizzbuzz.cpp

---
+++ fizzbuzz.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int number;
+	for (number = 1; number < 101; number++){
+		if (number%3!=0 && number%5!=0){
+            cout<<number;
+            cout<<"\n";
+		}
+		else if (number%3==0 && number%5!=0){
+            cout<<"Fizz ";
+            cout<<"\n";
+		}
+		else if (number%5==0 && number%3!=0){
+            cout<<"Buzz ";
+            cout<<"\n";
+        }
+        else if (number%3==0 && number%5==0){
+            cout<<"FizzBuzz ";
+            cout<<"\n";
+        }
+	}
+return 0;
+}
